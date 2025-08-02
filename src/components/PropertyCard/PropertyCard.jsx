fa6fdb5d8b562963a521090fb5306869febb7c9f
yudin-oleg <yudin.1996.oleg@gmail.com>
2025-08-02 21:59:32 +0300

    fixed proprty card styles

--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -7,26 +7,28 @@ function PropertyCard({ property }) {
 	return (
 		<div className="property-card">
 			<img src={property.image} alt="property card" />
-			<div className="title">{property.title}</div>
-			<div className="description">{property.description}</div>
-			<div className="house-guest">
-				<div className="house">
-					<House />
-					<p>{`${property.capacity.bedroom} bedroom`}</p>
+			<div className="card-main-part">
+				<div className="title">{property.title}</div>
+				<div className="description">{property.description}</div>
+				<div className="house-guest">
+					<div className="house">
+						<House />
+						<p>{`${property.capacity.bedroom} bedroom`}</p>
+					</div>
+					<div className="guest">
+						<Guest />
+						<p>{`${property.capacity.people} guests`}</p>
+					</div>
 				</div>
-				<div className="guest">
-					<Guest />
-					<p>{`${property.capacity.people} guests`}</p>
-				</div>
-			</div>
-			<div className="price-rating">
-				<div className="price-container">
-					<span className="price">{`$${property.price}`}</span>
-					<span className="price-night">/night</span>
-				</div>
-				<div className="rating-container">
-					<Star />
-					<div className="rating">{property.rating}</div>
+				<div className="price-rating">
+					<div className="price-container">
+						<span className="price">{`$${property.price}`}</span>
+						<span className="price-night">/night</span>
+					</div>
+					<div className="rating-container">
+						<Star />
+						<div className="rating">{property.rating}</div>
+					</div>
 				</div>
 			</div>
 		</div>
